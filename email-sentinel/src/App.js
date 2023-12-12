101e932f0ca882a3a643e9b47c0e0d2af2722fb4
Vansh-829 <vanshjagani2002@gmail.com>
2023-12-12 15:37:13 +0530

    initial commit

---
+++ email-sentinel/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Welcome to Email-Sentinel !!</h1>
+    <button>Authorize</button>
+    </div>
+  );
+}
+
+export default App;
